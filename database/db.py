f7085ed609b465ff7779f312d889e7939de087fd
Akhmet68 <zhalgasbaiakhmet@gmail.com>
2025-12-03 00:48:53 +0500

    1

--- database/db.py
+++ database/db.py
@@ -4,22 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("postgres")
+DB_USER = os.getenv("postgres")
+DB_PASSWORD = os.getenv("1234")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
 
 
 def get_connection():
-    conn = psycopg2.connect(
-        host=5432,
-        port=5432,
-        dbname=postgres,
-        user=postgres,
-        password=1234
+    return psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
     )
-    return conn
 
 
 def init_db():
@@ -34,7 +33,7 @@ def init_db():
             name TEXT,
             phone TEXT,
             notes TEXT
-        );
+        )
         """
     )
 
