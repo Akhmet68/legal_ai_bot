e45c9a1f196c75884c41f6fe2e04799b3468b71a
Akhmet68 <zhalgasbaiakhmet@gmail.com>
2025-12-03 00:39:55 +0500

    123

---
+++ bot.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+
+from config import TOKEN
+from database.db import init_db
+
+from handlers import start, help, questions, documents, templates, accident, client_card
+
+
+async def main():
+    # Инициализация БД
+    init_db()
+
+    bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+
+    # Подключаем роутеры
+    dp.include_router(start.router)
+    dp.include_router(help.router)
+    dp.include_router(questions.router)
+    dp.include_router(documents.router)
+    dp.include_router(templates.router)
+    dp.include_router(accident.router)
+    dp.include_router(client_card.router)
+
+    print("Бот запущен...")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
